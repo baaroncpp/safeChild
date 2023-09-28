6b7c11d6381eb18c451e428f0eb983243a73cf64
baaroncpp <baaronlubega1@gmail.com>
2023-09-28 19:47:44 +0300

    Add student and school web

--- core/src/test/java/com/bwongo/core/CoreApplicationTests.java
+++ core/src/test/java/com/bwongo/core/CoreApplicationTests.java
@@ -3,10 +3,9 @@ package com.bwongo.core;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CoreApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
