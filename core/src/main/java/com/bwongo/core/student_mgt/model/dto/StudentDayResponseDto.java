ae02e0d6d60323ec499868f99f5219bd93973328
baaroncpp <baaronlubega1@gmail.com>
2023-12-11 10:54:21 +0300

    Update swagger documentation

--- core/src/main/java/com/bwongo/core/student_mgt/model/dto/StudentDayResponseDto.java
+++ core/src/main/java/com/bwongo/core/student_mgt/model/dto/StudentDayResponseDto.java
@@ -2,12 +2,8 @@ package com.bwongo.core.student_mgt.model.dto;
 
 import com.bwongo.core.base.model.dto.LocationResponseDto;
 import com.bwongo.core.base.model.enums.StudentStatus;
-import com.bwongo.core.base.model.jpa.TLocation;
 import com.bwongo.core.school_mgt.model.dto.SchoolResponseDto;
-import com.bwongo.core.school_mgt.model.jpa.TSchool;
-import com.bwongo.core.student_mgt.model.jpa.TStudent;
 import com.bwongo.core.user_mgt.model.dto.UserResponseDto;
-import com.bwongo.core.user_mgt.model.jpa.TUser;
 
 import java.util.Date;
 
